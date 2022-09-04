edfb0110d09d34a57fcdb651bfe92217f626b949
Denis Toryansky <dtoryansky@gmail.com>
2022-09-04 18:19:21 +0300

    Task 8 was added.v.1

--- Task_8/Program_8.cs
+++ Task_8/Program_8.cs
@@ -1,9 +1,15 @@
 ﻿// Напишите программу, которая на вход принимает число (N), 
 // а на выходе показывает все чётные числа от 1 до N.
 
-Console.WriteLine("Введите число и нажмите Enter");
-int Number = Convert.ToInt32(Console.ReadLine());
+internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число и нажмите Enter");
+        int Number = Convert.ToInt32(Console.ReadLine());
 
-int i;
-for (i = 2; i <= Number; i += 2) 
-Console.WriteLine(i);
+        int i;
+        for (i = 2; i <= Number; i += 2)
+            Console.WriteLine(i);
+    }
+}
